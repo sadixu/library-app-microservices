d2b34cc0c8b490e2bbabac9d16cfe8f4a55e8dd5
Patryk Sadok <patryk.sadok@gmail.com>
2020-06-15 21:30:56 +0200

    Added example middleware

--- book-service/src/books/books.module.ts
+++ book-service/src/books/books.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common'
 import { Transport, ClientsModule } from '@nestjs/microservices'
 import { CqrsModule } from '@nestjs/cqrs'
 import { MongooseModule } from '@nestjs/mongoose'
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { BookService } from './services/book.service'
 import { BooksController } from './controllers/books.controller'
 import { BookSchema } from './schemas/Book'
+import { CreateBookMiddleware } from './controllers/middlewares/create-book.middleware'
 
 const { RMQ_USER, RMQ_PASSWORD, RMQ_PORT, RMQ_HOST, RMQ_VIRTUAL_HOST, RMQ_USER_QUEUE, SERVICE_NAME } = process.env
 const rmqConnectionUrl = `amqp://${RMQ_USER}:${RMQ_PASSWORD}@${RMQ_HOST}:${RMQ_PORT}/${RMQ_VIRTUAL_HOST}`
@@ -28,4 +29,8 @@ const rmqConnectionUrl = `amqp://${RMQ_USER}:${RMQ_PASSWORD}@${RMQ_HOST}:${RMQ_P
   controllers: [BooksController],
   providers: [BookService],
 })
-export class BooksModule {}
+export class BooksModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CreateBookMiddleware).forRoutes({ path: 'book', method: RequestMethod.POST })
+  }
+}
