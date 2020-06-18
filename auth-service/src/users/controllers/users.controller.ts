a0435b30632b26f9ef2d54ef142d89e2de5f8cbd
Patryk Sadok <patryk.sadok@gmail.com>
2020-06-18 22:08:17 +0200

    Cleaning users microservice

--- auth-service/src/users/controllers/users.controller.ts
+++ auth-service/src/users/controllers/users.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Inject, Get, Post, Res, Body, Logger } from '@nestjs/common'
 import { EventPattern, ClientProxy, MessagePattern, Payload, Ctx, RmqContext } from '@nestjs/microservices'
 
-import { CreateUserDTO } from '../dtos/create-user.dto'
-import { LoginUserDTO } from '../dtos/login-user.dto'
 import { UserService } from '../services/user.service'
-import { CommandHandlerNotFoundException } from '@nestjs/cqrs'
 
 const { SERVICE_NAME } = process.env
 
@@ -13,14 +10,6 @@ export class UsersController {
   constructor(@Inject(SERVICE_NAME) private readonly client: ClientProxy, private readonly service: UserService) {
     Logger.log('User controller is up and fresh.')
   }
-
-  @Post()
-  async registerUser(@Res() res, @Body() dto: CreateUserDTO) {
-    const result = await this.service.registerUser(dto)
-
-    return res.send(result)
-  }
-
 
   @MessagePattern('register-user')
   async registerUserEvent(@Payload() data: any, @Ctx() context: RmqContext) {
@@ -50,27 +39,5 @@ export class UsersController {
     } catch (error) {
       return { error }
     }
-  }
-
-  @EventPattern('message_printed')
-  async getNotifications(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log('4. dostalem message, ', data.text)
-
-    const channel = context.getChannelRef()
-    const originalMsg = context.getMessage()
-    channel.ack(originalMsg)
-    console.log('5. zrobilem acknowledge')
-    return 112
-  }
-
-  @EventPattern('message_printed2')
-  async getNotifications2(@Payload() data: any, @Ctx() context: RmqContext) {
-    console.log('4. dostalem message, ', data.text)
-
-    const channel = context.getChannelRef()
-    const originalMsg = context.getMessage()
-    channel.ack(originalMsg)
-    console.log('5. zrobilem acknowledge')
-    return 113
   }
 }
